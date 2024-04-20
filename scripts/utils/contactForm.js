aa0a7d9dea518b2255c269ef5719dc344e428ec4
Florian Bode <florianbode59@gmail.com>
2024-04-20 21:41:37 +0200

    implementation du formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,24 @@
 function displayModal() {
   const modal = document.getElementById('contact_modal');
-  modal.style.display = 'block';
+  modal.style.display = 'flex';
 }
 
 function closeModal() {
   const modal = document.getElementById('contact_modal');
   modal.style.display = 'none';
 }
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const firstname = document.getElementById('firstname').value;
+  const lastname = document.getElementById('lastname').value;
+  const email = document.getElementById('mail').value;
+  const message = document.getElementById('message').value;
+
+  console.log('Prénom:', firstname);
+  console.log('Nom:', lastname);
+  console.log('Email:', email);
+  console.log('Message:', message);
+});
