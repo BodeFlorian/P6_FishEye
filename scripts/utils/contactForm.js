b7b94aa7921509c65444c83f798d71b906fcfd61
Florian Bode <florianbode59@gmail.com>
2024-04-21 15:22:24 +0200

    accessibilité du formulaire de contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,14 +1,51 @@
+const originalTabIndexes = [];
+
+function disableFocusOnElementsOutsideModal() {
+  const focusableElements = document.querySelectorAll('body *:not(.modal *)');
+  focusableElements.forEach((element) => {
+    const tabIndex = element.getAttribute('tabindex');
+    originalTabIndexes.push({ element, tabIndex });
+    element.setAttribute('tabindex', '-1');
+  });
+}
+
+function restoreFocusOnElements() {
+  originalTabIndexes.forEach(({ element, tabIndex }) => {
+    if (tabIndex !== null) {
+      element.setAttribute('tabindex', tabIndex);
+    } else {
+      element.removeAttribute('tabindex');
+    }
+  });
+  originalTabIndexes.length = 0;
+}
+
 function displayModal() {
   const modal = document.getElementById('contact_modal');
+  const form = modal.querySelector('form');
+
+  disableFocusOnElementsOutsideModal();
+
   modal.style.display = 'flex';
+
+  form.getElementsByTagName('input')[0].focus();
 }
 
-function closeModal() {
-  const modal = document.getElementById('contact_modal');
-  modal.style.display = 'none';
+function closeModal(event) {
+  if (event.keyCode === 13 || event.type === 'click') {
+    const modal = document.getElementById('contact_modal');
+
+    restoreFocusOnElements();
+
+    modal.style.display = 'none';
+  }
 }
 
+const closeBTN = document.querySelector('#closeModal');
 const form = document.querySelector('form');
+
+closeBTN.addEventListener('keydown', closeModal);
+closeBTN.addEventListener('click', closeModal);
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -21,4 +58,6 @@ form.addEventListener('submit', function (event) {
   console.log('Nom:', lastname);
   console.log('Email:', email);
   console.log('Message:', message);
+
+  closeBTN.click();
 });
